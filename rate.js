0188aa31d2dd06f59064b331e5bfc55ed8fb3629
Chlending <artmart289@gmail.com>
2024-08-02 00:21:28 +0500

    Add files via upload

---
+++ rate.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+const CONVENTIONAL_RATE='6.375%'
+const CONVENTIONAL_APR="6.375%"
+
+const FHA_RATE="5.875%"
+const FHA_APR="6.736%"
+
+const VA_RATE="5.875%"
+const VA_APR="6.073%"
+
+
+
+document.getElementById("rate_conventional").textContent = CONVENTIONAL_RATE; 
+document.getElementById("term_conventional").textContent = CONVENTIONAL_APR; 
+
+document.getElementById("rate_fha").textContent = FHA_RATE; 
+document.getElementById("term_fha").textContent = FHA_APR; 
+
+
+document.getElementById("rate_va").textContent = VA_RATE; 
+document.getElementById("term_va").textContent = VA_APR; 
+
+console.log("script attached")
+
+});
