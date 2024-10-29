25d5499b9ab40d90a8ef507b9786e0ef44ff2106
Chlending <raulh32@gmail.com>
2024-10-29 09:28:34 -0500

    Update rates.js

--- rates.js
+++ rates.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-const CONVENTIONAL_RATE='6.08%'
-const CONVENTIONAL_APR="6.11%"
+const CONVENTIONAL_RATE='6.369%'
+const CONVENTIONAL_APR="6.4%"
 
-const FHA_RATE="5.747%"
-const FHA_APR="6.63%"
+const FHA_RATE="6.205%"
+const FHA_APR="7.08%"
 
-const VA_RATE="5.746%"
-const VA_APR="5.96%"
+const VA_RATE="6.125%"
+const VA_APR="6.35%"
 
-const DISCLAIMER = "*As of 10/21/2024. Rates based on the average purchase price of $525,000 and 740 credit score. 30yr fixed rates at 80% LTV for Conventional Loan - affordable loan program, 96.5% LTV for FHA Loan, and 100% LTV for VA loan."
+const DISCLAIMER = "*As of 10/29/2024. Rates based on the average purchase price of $525,000 and 740 credit score. 30yr fixed rates at 80% LTV for Conventional Loan - affordable loan program, 96.5% LTV for FHA Loan, and 100% LTV for VA loan."
 
 
 document.getElementById("rate_conventional").textContent = CONVENTIONAL_RATE; 
