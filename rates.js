685342f5c021b57269e2c6df6a11b1ad174b1c9c
Chlending <raulh32@gmail.com>
2026-03-09 13:37:39 -0500

    Update rates.js

--- rates.js
+++ rates.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
 // ISO format: YYYY-MM-DD
 window.CHL_UPDATED = '2026-03-09';
   
-const CONVENTIONAL_RATE='6.125%'
-const CONVENTIONAL_APR="6.156%"
+const CONVENTIONAL_RATE='5.99%'
+const CONVENTIONAL_APR="6.031%"
 
-const FHA_RATE="5.25%"
-const FHA_APR="6.15%"
+const FHA_RATE="5.125%"
+const FHA_APR="6.028%"
 
 const VA_RATE="5.625%"
 const VA_APR="5.844%"
@@ -32,6 +32,7 @@ document.getElementById("disc_paragraph").textContent =  DISCLAIMER;
 
 
 });
+
 
 
 
