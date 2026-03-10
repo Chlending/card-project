d251e854032f779d2b40b6814f04beb2d24f4d55
Chlending <raulh32@gmail.com>
2026-03-10 08:42:58 -0500

    Update rates.js

--- rates.js
+++ rates.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Stamp the last updated date for all pages that include rates.js
 // ISO format: YYYY-MM-DD
-window.CHL_UPDATED = '2026-03-09';
+window.CHL_UPDATED = '2026-03-10';
   
 const CONVENTIONAL_RATE='5.99%'
 const CONVENTIONAL_APR="6.031%"
@@ -32,6 +32,7 @@ document.getElementById("disc_paragraph").textContent =  DISCLAIMER;
 
 
 });
+
 
 
 
