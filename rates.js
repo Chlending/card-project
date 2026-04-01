7efe6c323d88b637eafa94397f9da40d36198990
Chlending <raulh32@gmail.com>
2026-04-01 08:48:47 -0500

    Update last updated date to 2026-04-01

--- rates.js
+++ rates.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Stamp the last updated date for all pages that include rates.js
 // ISO format: YYYY-MM-DD
-window.CHL_UPDATED = '2026-03-31';
+window.CHL_UPDATED = '2026-04-01';
   
 const CONVENTIONAL_RATE='6.375%'
 const CONVENTIONAL_APR="6.406%"
