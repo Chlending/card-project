b7f881108c233ebc877d2c3205be596e4fd3a1ec
Chlending <raulh32@gmail.com>
2026-04-14 17:12:04 -0500

    Update mortgage rates and APR values

--- rates.js
+++ rates.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
 // ISO format: YYYY-MM-DD
 window.CHL_UPDATED = '2026-04-14';
   
-const CONVENTIONAL_RATE='6.25%'
-const CONVENTIONAL_APR="6.281%"
+const CONVENTIONAL_RATE='6.125%'
+const CONVENTIONAL_APR="6.156%"
 
-const FHA_RATE="5.375%"
-const FHA_APR="6.272%"
+const FHA_RATE="5.25%"
+const FHA_APR="6.15%"
 
 const VA_RATE="5.625%"
 const VA_APR="5.844%"
