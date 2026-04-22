1f83bd22bd9ae37048cc7ca95617556754a3e55f
Chlending <raulh32@gmail.com>
2026-04-22 15:53:01 -0500

    Modify FHA rates and APR in rates.js
    Updated FHA rates and APR values.

--- rates.js
+++ rates.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const CONVENTIONAL_RATE = "6.25%";
   const CONVENTIONAL_APR = "6.281%";
 
-  const FHA_RATE = "5.25%";
-  const FHA_APR = "6.15%";
+  const FHA_RATE = "5.375%";
+  const FHA_APR = "6.272%";
 
   const VA_RATE = "5.625%";
   const VA_APR = "5.844%";
